a7c89551ab0190a167cb908ab6003aa6b81530d0
vcodezjanavi-collab <vcodez.janavi@gmail.com>
2025-12-23 16:43:43 +0530

    Initialize Streamlit app with welcome message

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Welcome ML Interns",
+    page_icon="🤖",
+    layout="centered"
+)
+
+st.title("Hi Janavi 👋")
+st.subheader("Welcome my dear ML Interns 🚀")
+
+)
