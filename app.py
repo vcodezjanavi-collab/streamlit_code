84109bd9aa89822107ccbe16245e97d6cda24722
vcodezjanavi-collab <vcodez.janavi@gmail.com>
2025-12-24 16:41:48 +0530

    Change welcome message from ML to DS Interns

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Welcome ML Interns",
+    page_title="Welcome DS Interns",
     page_icon="🤖",
     layout="centered"
 )
 
 st.title("Hi Janavi 👋")
-st.subheader("Welcome my dear ML Interns 🚀")
+st.subheader("Welcome my dear DS Interns 🚀")
 st.subheader("Learn Streamlit")
 
