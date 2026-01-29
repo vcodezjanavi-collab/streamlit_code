2be633dbe04fc2b8890d6cc6256028d93e3612db
vcodezjanavi-collab <vcodez.janavi@gmail.com>
2026-01-29 11:58:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(
     layout="centered"
 )
 
-st.title("Hi Janavi")
+st.title("Hi Sri Harieesh")
 st.subheader("Welcome my dear DS Interns 🚀")
 st.subheader("Learn Streamlit")
 st.subheader("Python Framework")
